6727c3a0d49811bf1ed77cfbcb2259df9658d2e8
IrukNuj <kurijune@gmail.com>
2018-02-13 11:40:35 +0900

    演算子いろいろ

--- 5_Operator.js
+++ 5_Operator.js
@@ -2,3 +2,14 @@ document.writeln(1+10);//そのまま
 document.writeln("1+10");//文字列だから
 var today = new Date();
 document.writeln(1234+today);//型が違う
+
+var x = 3;
+var y = x++;
+document.writeln(x);//4(y=3した後にx=x+1
+document.writeln(y);
+
+var x = 3;
+var y = ++x;
+document.writeln(x);
+document.writeln(y);
+
